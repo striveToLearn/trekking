8e6da2caa5b2481b05ba7d112a8a16b7640e3226
Debojit Bhattacharya <debojitdb1410@gmail.com>
2026-08-05 17:22:22 +0530

    Added shared modules and topbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopbarComponent } from './shared/topbar/topbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TopbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
