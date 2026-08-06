9b65cb897ee7a6490c120b49407fd16d65b50461
Debojit Bhattacharya <debojitdb1410@gmail.com>
2026-08-06 15:26:29 +0530

    Deployment not reflecting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TopbarComponent } from './shared/topbar/topbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: TopbarComponent}
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroComponent } from './pages/home/hero/hero.component';
+import { TopbarComponent } from './shared/topbar/topbar.component';
+import { TrekCategoriesComponent } from './pages/home/trek-categories/trek-categories.component';
+import { FeaturedTreksComponent } from './pages/home/featured-treks/featured-treks.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HeroComponent,
+    TopbarComponent,
+    TrekCategoriesComponent,
+    FeaturedTreksComponent
   ],
   imports: [
     BrowserModule,
